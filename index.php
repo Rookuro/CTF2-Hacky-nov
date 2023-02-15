4a73a66d3e345623879f4426fdb7fda350db8bc0
AGIROMAGNY <alexis.giromagny@ynov.com>
2023-02-15 09:43:09 +0100

    Creator(Project CTF2) Creation of project CTF2.

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang='en'>
+	<head>
+		<meta charset='UTF-8'>
+		<title>Hack Me Challenge</title>
+		<link rel='stylesheet' href='styles/index.css'>
+		<script src='scripts/wtfisthis.js'></script>
+	</head>
+	<body>
+		<div class="navbar">
+			<a href='pages/challenge1.php'>
+				<button>
+					Challenge 1
+				</button>
+			</a>
+			<a href='pages/challenge2.php'>
+				<button>
+					Challenge 2
+				</button>
+			</a>
+			<a href='pages/challenge3.php'>
+				<button>
+					Challenge 3
+				</button>
+			</a>
+		</div>
+
+		<div class="main">
+			<h1>Hack Me Challenge</h1>
+			<p>Your goal is to complete all the challenges in less than 30 minutes.</p>
+			<div class="difficulties">
+				<h2>Challenges difficulties</h2>
+				<ul>
+					<li>Challenge 1: 8/10</li>
+					<li>Challenge 2: 7/10</li>
+					<li>Challenge 3: 5/10</li>
+				</ul>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ pages/challenge1.php
@@ -0,0 +1,92 @@
+<?php
+$name = "";
+$cookieadmin = '$message' . " = " . '"challenge48H"' . ';';
+$message = "";
+
+setcookie('EUKFKZxcJD8ZU0WvWgw9IUI72h0k9EzVjDosYQ7S', '="FALSE"');
+setcookie('9IUI72h0k9EzVjDosYQ7SEUKFKZxcJD8ZU0WvWgw', '25="$MESSAGE"');
+setcookie('L9gnhUrT2kDinxYGOPiyIQd5GUS43ny4ZSgmCeTQ', '24="ch%a%ll"');
+setcookie('O3vg3kq7Rn2aLoTgBwXCKcyTpZkbLYOxbfoLLWec', '23="e%ng%e"');
+setcookie('YoT5x94SCiknhjkA4eSQECBl3KJSC8u0NNAeGZOQ', '22="4%8H%"');
+setcookie('fJ1NWIdqVnoPf15qyHlv5Dt887UZtoEOQVPcEgNE', '="FALSE"');
+setcookie('Ps9KFwzDwzhkosl8plqLD9aPsU91956dgyjK7Q0h', '="FALSE"');
+setcookie('ERPVT7ktKFViq2M4zU9mN6mKF72ZkNMgQNiaz3Aq', '="FALSE"');
+setcookie('NuhxOVRy7zMGgpAzyaXQjtzVAvKr6GWNipR4DnJH', '="FALSE"');
+setcookie('mdlVBRZsdECKuAMFxefUeDszPz6SAsBVQFaMk4FA', '="FALSE"');
+setcookie('6BXoFrAHjQLKlnS9oZF1DtViT5biF8nNmfoWaNEK', '="FALSE"');
+setcookie('sPS8f8i966k52ahg5OGvkGd1RDqZx7lfRWkcaxoy', '="FALSE"');
+setcookie('zegMZiVAvFO0tSotm3Vwgi2vNzxN5kkPIv2wYc9T', '="FALSE"');
+setcookie('pfMikcaHNVZjQpsA5PN4nFLPlCw6BPjRtkirJ5aY', '="FALSE"');
+setcookie('rtvE7aQasijBpZEPg2BpZpZ42puyilsAEvCgkBFX', '="FALSE"');
+setcookie('3j5H4JzcNoc8X7ETG77IKVmeOn56XbnyK04PUb5B', '="FALSE"');
+setcookie('L5WbIBA7mIhmvDwsZeAvZORkUizX87hI0ImKugvG', '="FALSE"');
+setcookie('36pGGQWMNUeQg1FoERsmTk8GCCFOkJVllcUto3Ha', '="FALSE"');
+setcookie('MF7RSunIfDhcJryL9CFKMfScLOAdmVvnboyFdIBS', '="FALSE"');
+setcookie('XRcq4pZbVGP75JaeU1yPOzVpBFm1O0ZCAzYQMSIZ', '="FALSE"');
+setcookie('PM4BoBAxj1CVAMzpI0N3IGHDNmPG7DD1lcIiWQFM', '="FALSE"');
+setcookie('evZZajXIXE6VXzbv6O1QT3W57DDzLt9r9c1Tva9G', '="FALSE"');
+setcookie('H6zXtrYkEOMFfauzBitqTODt98iwULeOMDnAGr05', '="FALSE"');
+setcookie('5YPGu8mwfAmUzrB0cBI88tJID00Lw7MyyPEgVpPE', '="FALSE"');
+setcookie('nMv2oy8HbQZgL8QJK4TjrmhAIyhm6bYtLTHEjhxa', '="FALSE"');
+setcookie('XREZW9Ek2KuHMsls8ntQNqds6Issbc9c6F8PKVx7', '="FALSE"');
+setcookie('6jvZBSRw3qtU2hDYN1lN8yShslRZD9ZrXv4qTMaT', '="FALSE"');
+setcookie('oiYxeEpU7nxn8KEnPdRzVPWqYfFixH3MNwAPEwIE', '="FALSE"');
+setcookie('KtGVhsETZkWAVrXLVD4vGAQQtfM0bAFSRkx8wTEY', '="FALSE"');
+setcookie('cFWjQuej7X6setzV6tRyQUAENmiWgAwXesRdSvcj', '="FALSE"');
+setcookie('2aYkzIqqLHGCqDXIV8DEo04Wj0W9mXNlog35JabA', '="FALSE"');
+setcookie('LT6mB0Iaw0CaEAv8CPESMvJqdcKxLvKeBDx26WR8', '="FALSE"');
+setcookie('RisCqAqzlWUyTKcU0TmiIYX6iT83Gi28W1YJpBue', '="FALSE"');
+setcookie('1n3i6SLG8cn0uuDCmDQvr3X54IaXb69gnwmb7WnP', '="FALSE"');
+setcookie('Oso4cFfkAlQi39SpAsQJX40UirVBxwRTJvqzOS19', '="FALSE"');
+setcookie('oc2Nd8TDpl623K52NVNycsXKju3807jrT2w0cChR', '="FALSE"');
+setcookie('ZfY5zbdg0YW5cLxdnfZgjIrClfXPGWyWMgsCxZ0F', '="FALSE"');
+setcookie('xt5JFvbyyHUZtMtEXOk9vphiF1DrotcUdKbHsrII', '="FALSE"');
+setcookie('eYOsESlH674kmi12Td0kaSAxQiUPfmvHaU2BMAiv', '="FALSE"');
+setcookie('lFt471LNyzSFzJwukIXBM4x9zZVC5GTb3c0a0tR6', '="FALSE"');
+setcookie('UEr7CvoJV4GIVzqrfNJiiGBhD941vSiEZ7dBt0Pe', '="FALSE"');
+setcookie('r2WRH916KczurfVisr7EcxUxQhQfi2mtvj3hUpt2', '="FALSE"');
+setcookie('t3nahxKB8Q12Zzi50QGBeLuOmzEjn3WtNULUPph4', '="FALSE"');
+setcookie('O6exw3mD9N95oRN4c84AC63w2EcdlHGdiSn690GV', '="FALSE"');
+setcookie('mj5iNy5vqJ5onEvccThDGSskHLQqxtejva9mjFPW', '="FALSE"');
+setcookie('ErNfbzYp7N10FdbtNuawtMZswrsMn1BeaSEYxm13', '="FALSE"');
+setcookie('1QrawJ074YZWJrj4zEPeiUWZ86ZkiiaXZTM57aRX', '="FALSE"');
+setcookie('Ul9X83Klqe5mWfKuSYutPxuZViD3aCSZV2RFh5OU', '="FALSE"');
+setcookie('M30pp8x4ogNvPwYsw8AuiIwDAI4Q0X7SguAe0Bw4', '="FALSE"');
+setcookie('q1OeMQfFXlvCLYGzrKmKNsdEOeTyyw3gfT977E2A', '="FALSE"');
+setcookie('IefOyRdPRxhhU6Tu0tY4APhwaUAs7qR71mLFFQL4', '="FALSE"');
+?>
+<!DOCTYPE html>
+<html lang='en'>
+	<head>
+		<title>Challenge 1</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="../styles/challenge-1.css">
+		<link rel="stylesheet" href="../styles/index.css">
+		<script src="../scripts/wtfisthis.js"></script>
+	</head>
+
+	<body>
+		<?php
+		if (isset($_POST['bouton'])) {
+			$message = $_POST['message'];
+		}
+		?>
+		<div class="container">
+			<form class="password-form" method="POST" action="challenge1.php">
+				<div class="pageTitle">Challenge 1</div>
+				<div class="secondaryTitle">Please complet the form.</div>
+				<input type="text" class="code name formEntry" placeholder="Name" name="user_name"/>
+				<input type="text" class="code email formEntry" placeholder="Email" name="email"/>
+				<input class="code message formEntry" placeholder="Message" name="message"/>
+				<div class="center">
+					<?php
+					if ($cookieadmin == $message) {
+						echo "<a href='123.png'>Here it is.</a>";
+					}
+					?>
+				</div>
+				<button class="formEntry" name="bouton">Submit</button>
+			</form>
+		</div>
+	</body>
+</html>
